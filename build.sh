7c18fd90aa7d1d093eb2bf957c35c299a34cecc3
Mac-A-4 <malcolm.alvarez9898@gmail.com>
2023-01-06 11:29:40 -0800

    Add publish to shell script

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 docker stop "sharepad2-service"
 docker rm "sharepad2-service"
 docker build --no-cache -t "sharepad2-service" .
-docker run -d --restart unless-stopped --name="sharepad2-service" "sharepad2-service"
+docker run -d --restart unless-stopped -p 3000:80 --name="sharepad2-service" "sharepad2-service"
